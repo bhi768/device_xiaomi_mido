11cc2cb06d0faebb323326f8ebd1e8b591af7b77
sathish2026 <txt2sathish@gmail.com>
2017-12-21 09:29:41 +0100

    Mido: Enable ANT

--- device.mk
+++ device.mk
@@ -64,7 +64,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # ANT
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library \
     libantradio
