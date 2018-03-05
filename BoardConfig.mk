14e8363f682f7cb9b0df2184c73623952d4059d3
rohitporoli <rohitp4all@gmail.com>
2018-03-05 23:29:38 +0530

    Revert "mido: Dexpreopt"
    This reverts commit ddee286eae7fe756320fd88985d0c070496a7139.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,7 +119,14 @@ ENABLE_CPUSETS := true
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Dexpreopt
-WITH_DEXPREOPT := true
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
