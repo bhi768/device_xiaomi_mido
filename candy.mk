54fae90e2f07a2c97d165d6d69a50ec7888263cf
bhi768 <bhimanand7@gmail.com>
2018-03-06 04:58:46 +0000

    Candy Bringup

--- candy.mk
+++ candy.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosip_mido
+PRODUCT_NAME := candy_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
