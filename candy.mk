d6547189874bdf2ffd0add8bc75fc6135f81c315
bhi768 <bhimanand7@gmail.com>
2018-03-08 07:11:20 +0000

    Mido: Candy Bringup

--- candy.mk
+++ candy.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some Bird shit.
-$(call inherit-product, vendor/cardinal/common.mk)
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
-PRODUCT_NAME := cardinal_mido
+PRODUCT_NAME := candy_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
