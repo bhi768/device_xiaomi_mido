6fca7e6b1d277a18688954ba7ef993fbd858db5d
bhi768 <bhimanand7@gmail.com>
2019-01-13 08:22:52 +0000

    Disable XiaomiParts

--- device.mk
+++ device.mk
@@ -151,8 +151,8 @@ PRODUCT_PACKAGES += \
     libtinyxml
 
 # Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+#PRODUCT_PACKAGES += \
+#    XiaomiParts
 
 # DRM
 PRODUCT_PACKAGES += \
